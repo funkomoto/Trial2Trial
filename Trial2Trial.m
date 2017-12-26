20b413b93588da578b2f3496fe5adf9f51ff61d5
funkomoto <some01@gmail.com>
2017-12-26 00:27:12 -0800

    Add files via upload
    Moved parsing code into functions
    
    Program now reads parameters, then jumps to the end and reads summary data (rather than checking every line for everything)

--- Trial2Trial.m
+++ Trial2Trial.m
@@ -1,22 +1,45 @@
 format shortG
-PathStr='C:\Users\Dan\Documents\Sutter Lab\Training\MATLAB\qtip\Q2\'; %%File path
-textFiles=dir([PathStr '*.log']); %%File Name
-for k = 1:length(textFiles) %%Cycles through individual files (k is filenumber
-    textFilename = [PathStr 'q' num2str(k) '.log']; 
-    fid = fopen(textFilename,'rt'); %%Opens file
-    if exist(textFilename, 'file') == 0 %Checks if the file exists
+
+%File path
+PathStr='C:\Users\Dan\Documents\Sutter Lab\Training\MATLAB\qtip\Q2\';
+
+%File Name
+textFiles=dir([PathStr '*.log']);
+
+
+fidLine = '';
+MyDataParam = [];
+
+%Cycles through individual files (k is filenumber)
+for k = 1:length(textFiles)
+    textFilename = [PathStr 'q' num2str(k) '.log'];
+    
+    %Opens file
+    fid = fopen(textFilename,'rt');
+    
+    %Checks if the file exists
+    if ~exist(textFilename, 'file') 
         MyDataParam(:,k) = NaN;
         MyDataParam(:,k) = NaN;
         continue;
     end
-    notfound1 = 1;
-    notfound2 = 1;
-    endfile = 0;
-    while ~feof(fid) %%cycles through lines in individual file
+    k
+    %Gets parameters from beginning of each file
+    MyDataParam = getParam(MyDataParam,fid,k);
+    MyDataParam = getTrial(MyDataParam,fid);
+    MyDataParam = getSumm(MyDataParam,fid,k);
+    fclose(fid);
+end
+
+%Displays completed Matrix
+MyDataParam
+
+%Gets parameters from beginning of each file
+function [MyDataParam] = getParam(MyDataParam,fid,k)
+    while 1
         fidLine = fgetl(fid); %%Get next line in individual file
-        if contains(fidLine,'payhvalue	') 
+        if contains(fidLine,'payhvalue	')
             payhvalue = getNumber2endnogetl('payhvalue',fidLine);
-            qtip.payhvalue = payhvalue;
             MyDataParam(1,k) = str2num(payhvalue);
 
         elseif contains(fidLine,'ythreshinner	')
@@ -99,12 +122,32 @@ for k = 1:length(textFiles) %%Cycles through individual files (k is filenumber
             trialonsetdelay = getNumber2endnogetl('trialonsetdelay',fidLine);
             MyDataParam(24,k) = str2num(trialonsetdelay);
             
-        elseif contains(fidLine,' number of wrong-axis interrupts ') %%Pulls out three numbers (rewards given, wrong-axis interrupts and delay interrupts)
+        elseif contains(fidLine,'end	end')
+            break;
+        end
+    end
+end
+
+%Skips to Summary
+function [MyDataParam] = getTrial(MyDataParam, fid)
+    while 1
+        fidLine = fgetl(fid);
+        if strcmp(fidLine,'The experiment is complete!')
+            break;
+        end
+    end
+end
+
+%Gets summary data from end of each file
+function [MyDataParam] = getSumm(MyDataParam,fid,k)
+    while ~feof(fid) %%cycles through lines in individual file
+        fidLine = fgetl(fid);
+        if contains(fidLine,' number of wrong-axis interrupts ') %%Pulls out three numbers (rewards given, wrong-axis interrupts and delay interrupts)
             RewardsLine = getNumbernogetl(' is ',',',fidLine);
             MyDataParam(25,k) = RewardsLine(1); %Rewards Given
             MyDataParam(26,k) = RewardsLine(2); %WrongAxisInterrupts
             MyDataParam(27,k) = RewardsLine(3); %DelayInterrupts
-            
+
         elseif contains (fidLine,'Final Up/Down inner threshold is')
             summline2 = getNumber(' is ',' degrees',fidLine);
             [UD,LR] = strtok(summline2,' ');
@@ -116,7 +159,7 @@ for k = 1:length(textFiles) %%Cycles through individual files (k is filenumber
             MyDataParam(29,k) = str2num(UpDownInnerThreshSecond);
             MyDataParam(30,k) = str2num(LRInnerThreshFirst);
             MyDataParam(31,k) = str2num(LRInnerThreshSecond);
-            
+
         elseif contains (fidLine,'Final Up/Down outer threshold is')
             summline3 = getNumber(' is ',' degrees',fidLine);
             [UD3,LR3] = strtok(summline3,' ');
@@ -128,11 +171,11 @@ for k = 1:length(textFiles) %%Cycles through individual files (k is filenumber
             MyDataParam(33,k) = str2num(UpDownOuterThreshSecond);
             MyDataParam(34,k) = str2num(LROuterThreshFirst);
             MyDataParam(35,k) = str2num(LROuterThreshSecond);
-            
+
         elseif contains (fidLine,'Final required delay is ')
             FinalReqDelay = getNumbernogetl(' is ', ' seconds', fidLine);
             MyDataParam(36,k) = FinalReqDelay;
-            
+
         elseif contains (fidLine,'Total Perfect Trials (hit before error = ')
             totalerrors = getNumbernogetl('Total errors = ',' (up = ',fidLine,1);
             summlineup = getNumbernogetl(' (up = ',', down = ',fidLine);
@@ -144,7 +187,7 @@ for k = 1:length(textFiles) %%Cycles through individual files (k is filenumber
             MyDataParam(40,k) = perfecttrials;
             MyDataParam(41,k) = summlineup(2);
             MyDataParam(42,k) = summlinedown(2);
-            
+
         elseif contains (fidLine,' overall) has been completed.  Response Window is ')
             trialnumber = getNumbernogetl(' (',' overall) has been completed.  ',fidLine);
             responsewindow = getNumbernogetl('Response Window is ','. Total accuracy is ',fidLine);
@@ -178,28 +221,23 @@ for k = 1:length(textFiles) %%Cycles through individual files (k is filenumber
             MyDataParam(56,k) = str2num(downaccuracy);
         end
     end
-    fclose(fid);
 end
 
-numfiles = size(MyDataParam);
-MyDataParam
-%%Data Analysis
 
-% Plot 1
-% notfound = 1;
-% figure
-% subplot(2,1,1)
-% scatter(MyData2(15,35:end),MyData2(22,35:end))
-% title('Interrupt Penalty vs Wrong Axis interrupts');
-% xlabel('Interrupt Penalty');
-% ylabel('Wrong Axis Interrupt');
-% 
-% subplot(2,1,2)
-% scatter(MyData2(16,35:end),MyData2(22,35:end));
-% title('Continuous Reinforcement Delay vs Wrong Axis Interrupt');
-% xlabel('Continuous Reinforcement Delay');
-% ylabel('Wrong Axis Interrupt');
 
+%Helpful functions
+%Pulls out number from each line
+function [GotNumber] = getNumber2endnogetl(SPosition,fidLine)
+if isempty(fidLine)
+    GotNumber = '';
+else
+    StartPos = strfind(fidLine, SPosition);
+    if fidLine(end) == '.'
+        StartPos = (StartPos - 1);
+    end
+    GotNumber = fidLine((StartPos+length(SPosition)+1):end);
+end
+end
 function [GotNumber] = getNumber(SPosition,EPosition,fid)
 GotNumber = '';
 if isempty(fid)
@@ -236,20 +274,3 @@ else
     end
 end
 end
-function [GotNumber] = getNumber2endnogetl(SPosition,fidLine)
-if isempty(fidLine)
-    GotNumber = '';
-else
-    StartPos = strfind(fidLine, SPosition);
-    if fidLine(end) == '.'
-        StartPos = (StartPos - 1);
-    end
-    GotNumber = fidLine((StartPos+length(SPosition)+1):end);
-end
-end
-
-%Notes
-%To Do
-    %Turn variables into structures
-        %Add info such as param file name, trial number, data type, etc.
-    %Add more graphs
